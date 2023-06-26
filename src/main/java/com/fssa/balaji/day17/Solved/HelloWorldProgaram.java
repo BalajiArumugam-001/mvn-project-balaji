35c40f3e1583c827f904f3cfc7400a3a8c188622
Balaji Arumugam <57753452+BalajiArumugam-001@users.noreply.github.com>
2023-06-26 23:43:18 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/balaji/day17/Solved/HelloWorldProgaram.java
@@ -0,0 +1,10 @@
+package com.fssa.balaji.day17.Solved;
+
+public class HelloWorldProgaram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+            System.out.println("Hello World");
+	}
+
+}
--- src/test/java/com/fssa/balaji/sampleprogs/day17/solved/HelloTest.java
+++ src/test/java/com/fssa/balaji/sampleprogs/day17/solved/HelloTest.java
@@ -1,5 +1,23 @@
 package com.fssa.balaji.sampleprogs.day17.solved;
 
-public class HelloTest {
 
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Sample test class
+ * @author BharathwajSoundarara
+ *
+ */
+public class HelloTest {
+	
+	/**
+	 * First test case
+	 */
+	@Test
+	public void helloTest() {
+		
+		Assertions.assertEquals("Hello World", "Hello World");
+	}
 }
